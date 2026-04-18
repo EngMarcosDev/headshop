ba2e0abdc8420c223147e0ff6f699f514cab03d0
NunessSantos <119435607+NunessSantos@users.noreply.github.com>
2026-04-18 12:22:03 -0300

    fix: Instagram

--- src/lib/socialLinks.ts
+++ src/lib/socialLinks.ts
@@ -3,4 +3,4 @@ export const CONTACT_WHATSAPP_PHONE = "5581981705445";
 export const CONTACT_WHATSAPP_DISPLAY = "(81) 98170-5445";
 export const CONTACT_WHATSAPP_URL = `https://wa.me/${CONTACT_WHATSAPP_PHONE}`;
 export const CONTACT_INSTAGRAM_URL =
-  "https://www.instagram.com/abacaxitashop?igsh=N2NncGpidDg4NmJq&utm_source=qr";
+  "https://www.instagram.com/abacaxitaheadshop/";
